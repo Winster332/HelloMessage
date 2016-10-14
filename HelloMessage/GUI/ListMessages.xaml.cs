fb441c7fb5a42aed24cb09e2678c37887220d584
Stas Martynov <stas-martynov-98@mail.ru>
2016-10-15 00:06:26 +0300

    Create first UI interface

---
+++ HelloMessage/GUI/ListMessages.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloMessage.GUI
+{
+	/// <summary>
+	/// Логика взаимодействия для ListMessages.xaml
+	/// </summary>
+	public partial class ListMessages : UserControl
+	{
+		public ListMessages()
+		{
+			InitializeComponent();
+		}
+	}
+}
